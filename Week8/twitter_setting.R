4c9d24ed42831860f0718c6d9a25264ff5b8fff4
Xiangdong Gu <xgu@massmutual.com>
2016-03-06 11:15:59 -0500

    week8

--- Week8/twitter_setting.R
+++
@@ -1,6 +0,0 @@
-consumer_key <- "3yQNVIZCgjVEX191m1qKhbiIu"
-consumer_secret <- "kfT6bbKlwXh4HDFPUhLyUPq9sMuD6oWzJPEbJFC6vtPlKomqxV"
-access_token <- "960801247-keWx3HmZDJEmfV54WFd4nGd4cHvIUykSl5mSgnrj"
-access_secret <- "WfCPyqBQIPchZewvwBWTsp7RB6VYMZmDmPuSvHuKVIAz2"
-
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
